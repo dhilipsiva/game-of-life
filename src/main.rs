25219d07ba905b36d9d94fb8d1b772f4bc5f9739
dhilipsiva <dhilipsiva@pm.me>
2021-12-29 23:07:24 +0530

    Initialize random seed state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::vec::Vec;
+
+type StateMatrix = Vec<Vec<bool>>;
+
+fn seed_state(row: usize, col: usize) -> StateMatrix {
+    let mut state = Vec::with_capacity(row);
+    for _ in 0..row {
+        let mut col_state = Vec::with_capacity(col);
+        for _ in 0..col {
+            col_state.push(rand::random())
+        }
+        state.push(col_state)
+    }
+    return state;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let row: usize = 5;
+    let col: usize = 5;
+    let state: StateMatrix = seed_state(row, col);
+    println!("state: {:X?}", state);
 }
